ca6ca87e6aff1f6dd2c93cf24ec31203dc7e3be2
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-23 18:56:11 +0100

    added various songs and began to code the chosing menu

--- main.js
+++ main.js
@@ -8,24 +8,46 @@ function main(){
 }
 
 function addEventListeners(){
-    document.getElementById('startbtn').addEventListener('click', startconcert);
-}
-
-function startconcert(){
-
-document.getElementById('startscreen').classList.add('startscreenfade')
-document.getElementById('startscreen').style.display = 'none';
-document.getElementById('drumcontainer').classList.add('drumcontainer')
-document.getElementById('drumcontainer').style.display = 'unset';
-rocktrack.play();
+    document.getElementById('nirvana').addEventListener('click', nirvana);
 
 }
 
-let rocktrack = new Audio();
-rocktrack.src="./assets/rock.mp3"
+function nirvana(){
+document.getElementById('chosesong').style.display = 'none';
+document.getElementById('chosetype').style.display = 'flex';
+
+let nodrums = document.getElementById('withoutdrums');
+nodrums.setAttribute("id", "nirvananodrums");
+document.getElementById('nirvananodrums').addEventListener('click', nirvanaNoDrums)
+
+
+let withDrums = document.getElementById('withdrums');
+withDrums.setAttribute("id", "nirvanawithdrums");
+document.getElementById('nirvanawithdrums').addEventListener('click', nirvanaWithDrums)
+}
+
+
+function nirvanaNoDrums(){
+    nirvananodrums.play();
+    console.log('utan trummor')
+}
+
+function nirvanaWithDrums(){
+    nirvanawithdrums.play();
+    console.log('med trummor')
+}
+
+//songs
+let nirvananodrums = new Audio();
+nirvananodrums.src="./assets/nodrumsnirvana.mp3";
+
+let nirvanawithdrums = new Audio();
+nirvanawithdrums.src="./assets/nirvana.mp3";
+
+
 
 let kicksample = new Audio();
-kicksample.src="./assets/kick.wav";
+kicksample.src="./assets/kick.mp3";
 
 let snaresample = new Audio();
 snaresample.src="./assets/snare.wav";
@@ -36,6 +58,11 @@ hihatsample.src="./assets/hihat.wav";
 let crashsample = new Audio();
 crashsample.src="./assets/crash.wav";
 
+let hightom = new Audio();
+hightom.src="./assets/hightom.wav";
+
+let lowtom = new Audio();
+lowtom.src="./assets/lowtom.wav";
 
 
 
@@ -43,31 +70,47 @@ function checkKeyPress(key, keyCode){
     if (key.keyCode == "65"){ // A
         kicksample.currentTime = 0;
         kicksample.play();
+        kicksample.currentTime = 0;
         document.getElementById('a-button').classList.add('kick-btn');
     }
     if (key.keyCode == "83"){ // S
         snaresample.currentTime = 0;
         snaresample.play();
+        kicksample.currentTime = 0;
         document.getElementById('s-button').classList.add('snare-btn');
     }
 
-    if (key.keyCode == "75" ){ // K
+    if (key.keyCode == "68"){ // D
+        hightom.currentTime = 0;
+        hightom.play();
+        document.getElementById('d-button').classList.add('hightom-btn');
+        
+    }
+
+    if (key.keyCode == "70"){ // F
+        lowtom.currentTime = 0;
+        lowtom.play();
+        document.getElementById('f-button').classList.add('lowtom-btn');
+        
+    }
+
+    if (key.keyCode == "74" ){ // J
+        hihatsample.currentTime = 0;
+        hihatsample.play();
+        document.getElementById('j-button').classList.add('hihat-btn');
+    }
+
+
+    if (key.keyCode == "75"){ // K
         hihatsample.currentTime = 0;
         hihatsample.play();
         document.getElementById('k-button').classList.add('hihat-btn');
     }
 
-
     if (key.keyCode == "76"){ // L
-        hihatsample.currentTime = 0;
-        hihatsample.play();
-        document.getElementById('l-button').classList.add('hihat-btn');
-    }
-
-    if (key.keyCode == "79"){ // O
         crashsample.currentTime = 0;
         crashsample.play();
-        document.getElementById('o-button').classList.add('crash-btn');
+        document.getElementById('l-button').classList.add('crash-btn');
     }
     
 } 
@@ -76,32 +119,50 @@ function checkKeyPress(key, keyCode){
 
 document.addEventListener("keyup", function(key, keyCode){
     if (key.keyCode == "65"){ // A
-;
+        kicksample.pause();
         document.getElementById('a-button').classList.remove('kick-btn');
      
     }
 
     if (key.keyCode == "83"){ // S
 
+        snaresample.pause();
         document.getElementById('s-button').classList.remove('snare-btn');
+        
+    }
+
+    if (key.keyCode == "68"){ // D
+
+
+        document.getElementById('d-button').classList.remove('hightom-btn');
+        
+    }
+
+    if (key.keyCode == "70"){ // F
+ 
+
+        document.getElementById('f-button').classList.remove('lowtom-btn');
+        
+    }
+
+    if (key.keyCode == "74"){ // J
+
+        hihatsample.pause();
+        document.getElementById('j-button').classList.remove('hihat-btn');
         
     }
 
     if (key.keyCode == "75"){ // K
 
+        hihatsample.pause();
         document.getElementById('k-button').classList.remove('hihat-btn');
         
     }
 
     if (key.keyCode == "76"){ // L
 
-        document.getElementById('l-button').classList.remove('hihat-btn');
-        
-    }
 
-    if (key.keyCode == "79"){ // O
-
-        document.getElementById('o-button').classList.remove('crash-btn');
+        document.getElementById('l-button').classList.remove('crash-btn');
     }
 
 
